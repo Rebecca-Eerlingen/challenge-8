f2ee58bed4021357d7f1e7b586370e13f1f05c22
jasper van kalsbeek <jasper.v.kalsbeek@gmail.com>
2026-03-12 09:03:16 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   public/index.php

--- public/index.php
+++ public/index.php
@@ -17,6 +17,29 @@ error_reporting (E_ALL); ?>
 
 </header>
 
+<body>
+<div class="container pokedex">
+    
+</div>
+
+     <?php 
+     $result = $conn->query("SELECT * FROM tb_pokemon");
+     $data = $result->fetchAll(PDO::FETCH_ASSOC);
+     foreach ($data as $row) { ?>
+            <tr>
+            <td><?= htmlspecialchars($row['dex_number']    ?? '—') ?></td>
+            <td><?= htmlspecialchars($row['name']  ?? '—') ?></td>
+            <td> <img src="../pokemon img/icons/<?= htmlspecialchars($row['dex_number']  ?? '—') ?>.png" alt=""></td>
+            <td><?= htmlspecialchars($row['type1'] ?? '—') ?></td>
+            <td><?= htmlspecialchars($row['type2'] ?? '—') ?></td>
+            <td><?= htmlspecialchars($row['weight'] ?? '—') ?></td>
+            <td><?= htmlspecialchars($row['height'] ?? '—') ?></td>
+            <td><?= htmlspecialchars($row['description'] ?? '—') ?></td> 
+<?php } ?>
+
+</a>
+</body>
+
 <footer>
     <?php include '../includes/footer.html'; ?>
 </footer>
