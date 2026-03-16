c48c575501cc0d48e9e336105bfdeb8191a48fdb
Rebecca-Eerlingen <519561@Vistacollege.nl>
2026-03-16 12:53:41 +0100

    Add files via upload

--- overzicht & detail/overzicht-cards.php
+++ overzicht & detail/overzicht-cards.php
@@ -11,7 +11,7 @@
 <div class="card-container">
     <div class="card">
         <h2 class="name"> pikachu <h2>
-        <div class="card-img"> </div>
+        <img class="card-img" src="https://i.pinimg.com/736x/05/3f/50/053f50fe48cbc691148b89906c37ddb1.jpg">
         <div class="card-body">
             <h3>Electric</h3>
             <p>Pikachu is an Electric type Pokémon introduced in Generation 1.</p>
@@ -21,7 +21,7 @@
 
     <div class="card">
         <h2 class="name"> Ivysaur <h2>
-        <div class="card-img"></div>
+        <img class="card-img" src="https://i.pinimg.com/736x/5b/3e/f4/5b3ef42890e16fd39d8185c41577ec19.jpg">
         <div class="card-body">
             <h3>Grass/Poison</h3>
             <p>Ivysaur is a Grass/Poison type Pokémon introduced in Generation 1.</p>
@@ -31,7 +31,7 @@
 
     <div class="card">
         <h2 class="name"> Bulbasaur <h2>
-        <div class="card-img"></div>
+        <img class="card-img" src="https://i.pinimg.com/736x/89/c6/00/89c60046db1da0bb99055eb55e599b11.jpg">
         <div class="card-body">
             <h3>Grass/Poison</h3>
             <p>Bulbasaur is a Grass/Poison type Pokémon introduced in Generation 1.</p>
@@ -41,7 +41,7 @@
 
     <div class="card">
         <h2 class="name"> Charmander <h2>
-        <div class="card-img"></div>
+        <img class="card-img" src="https://i.pinimg.com/736x/ba/8a/79/ba8a793afa1679a6dd4dc57d90c7d44f.jpg">
         <div class="card-body">
             <h3>Fire</h3>
             <p>Charmander is a Fire type Pokémon introduced in Generation 1.</p>
@@ -51,7 +51,7 @@
 
     <div class="card">
         <h2 class="name"> Charmeleon <h2>
-        <div class="card-img"></div>
+        <img class="card-img" src="https://i.pinimg.com/736x/21/ff/b6/21ffb63af76de2d513a3ac9da4419f88.jpg">
         <div class="card-body">
             <h3>Fire</h3>
             <p>Charmeleon is a Fire type Pokémon introduced in Generation 1.</p>
@@ -61,7 +61,7 @@
 
     <div class="card">
         <h2 class="name"> Squirtle <h2>
-        <div class="card-img"></div>
+        <img class="card-img" src="https://i.pinimg.com/1200x/53/e2/1a/53e21afd8d22d17a975363767536029f.jpg">
         <div class="card-body">
             <h3>Water</h3>
             <p>Squirtle is a Water type Pokémon introduced in Generation 1.</p>
