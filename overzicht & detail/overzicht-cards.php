576a837b16d64f569304f437326f2786dfd1634b
Rebecca-Eerlingen <519561@Vistacollege.nl>
2026-03-16 11:23:19 +0100

    Create overzicht-cards.php

---
+++ overzicht & detail/overzicht-cards.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>overzicht en detail</title>
+  <link rel="stylesheet" href="overzicht-cards.css">
+</head>
+<body>
+
+<div class="wrapper">
+<div class="card-container">
+    <div class="card">
+        <h2 class="name"> pikachu <h2>
+        <div class="card-img"> </div>
+        <div class="card-body">
+            <h3>Electric</h3>
+            <p>Pikachu is an Electric type Pokémon introduced in Generation 1.</p>
+            <a class="button" href="https://pokemondb.net/pokedex/pikachu"> More information </a>
+    </div>
+  </div>
+
+    <div class="card">
+        <h2 class="name"> Ivysaur <h2>
+        <div class="card-img"></div>
+        <div class="card-body">
+            <h3>Grass/Poison</h3>
+            <p>Ivysaur is a Grass/Poison type Pokémon introduced in Generation 1.</p>
+        <a class="button" href="https://pokemondb.net/pokedex/ivysaur"> More information </a>
+    </div>
+  </div>
+
+    <div class="card">
+        <h2 class="name"> Bulbasaur <h2>
+        <div class="card-img"></div>
+        <div class="card-body">
+            <h3>Grass/Poison</h3>
+            <p>Bulbasaur is a Grass/Poison type Pokémon introduced in Generation 1.</p>
+        <a class="button" href="https://pokemondb.net/pokedex/bulbasaur"> More information </a>
+        </div>
+    </div>
+
+    <div class="card">
+        <h2 class="name"> Charmander <h2>
+        <div class="card-img"></div>
+        <div class="card-body">
+            <h3>Fire</h3>
+            <p>Charmander is a Fire type Pokémon introduced in Generation 1.</p>
+        <a class="button" href="https://pokemondb.net/pokedex/charmander"> More information </a>
+        </div>
+     </div>
+
+    <div class="card">
+        <h2 class="name"> Charmeleon <h2>
+        <div class="card-img"></div>
+        <div class="card-body">
+            <h3>Fire</h3>
+            <p>Charmeleon is a Fire type Pokémon introduced in Generation 1.</p>
+        <a class="button" href="https://pokemondb.net/pokedex/charmeleon"> More information </a>
+            </div>
+    </div>
+
+    <div class="card">
+        <h2 class="name"> Squirtle <h2>
+        <div class="card-img"></div>
+        <div class="card-body">
+            <h3>Water</h3>
+            <p>Squirtle is a Water type Pokémon introduced in Generation 1.</p>
+        <a href="https://pokemondb.net/pokedex/squirtle"> More information </a>
+    </div>
+  </div>
+
+</div>
+</div>
+
+</body>
+</html>
