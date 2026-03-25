868bc50ac63ff1ac3e3ae33c2df9560bf64f295a
Rebecca-Eerlingen <519561@Vistacollege.nl>
2026-03-25 11:39:44 +0100

    Add files via upload

---
+++ overzicht & detail/pokemon-detail.php
@@ -0,0 +1,40 @@
+<?php 
+include ('../includes/db.php');
+
+$sql = "SELECT * FROM tb_pokemon WHERE dex_number = :dex_number";
+
+$stmt = $conn->prepare($sql);
+
+$stmt->execute(['dex_number' => $_GET['dex_number']]);
+
+$pokemon = $stmt->fetch();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>overzicht en detail</title>
+  <link rel="stylesheet" href="overzicht-cards.css">
+</head>
+<body>
+
+<div class="wrapper">
+    <div class="card-container">
+        <div class="card">
+            <h2 class="name"> <?= htmlspecialchars($pokemon['name']) ?> <h2>
+            <img src="../pokemon img/icons/<?= htmlspecialchars($row['dex_number']  ?? '—') ?>.png" alt="">
+            <div class="card-body">
+                <p> <?= htmlspecialchars($pokemon['description']) ?> </p>
+                <p> <?= htmlspecialchars($pokemon['type1']) ?> </P>
+                <p> <?= htmlspecialchars($pokemon['type2']) ?> </P>
+                <p> <?= htmlspecialchars($pokemon['weight']) ?> </P>
+                <p> <?= htmlspecialchars($pokemon['height']) ?> </P>
+                <a class="button" href="https://pokemondb.net"> More information </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
