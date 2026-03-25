ca4b77a6129732ea0d5d3a4a5b2d8cf4acfdeba8
jasper van kalsbeek <jasper.v.kalsbeek@gmail.com>
2026-03-25 17:10:02 +0100

    modified:   overzicht & detail/overzicht-cards.php

--- overzicht & detail/overzicht-cards.php
+++ overzicht & detail/overzicht-cards.php
@@ -1,3 +1,12 @@
+
+<?php
+session_start();
+include '../includes/db.php';
+$stmt = $conn->prepare("SELECT * FROM tb_pokemon");
+$stmt->execute();
+$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,93 +17,39 @@
 <body>
 
 <div class="wrapper">
-<div class="card-container">
-    <div class="card">
-        <h2 class="name"> pikachu <h2>
-        <img class="card-img" src="https://i.pinimg.com/736x/05/3f/50/053f50fe48cbc691148b89906c37ddb1.jpg">
-        <div class="card-body">
-            <h3>Electric</h3>
-            <p>Pikachu stores electricity in its cheeks and releases it when threatened.</p>
-            <p> Type 1: Electric </P>
-            <p> Type 2: - </P>
-            <p> Height: 0.4m </P>
-            <p> Weight: 6kg </P>
-            <a class="button" href="https://pokemondb.net/pokedex/pikachu"> More information </a>
-    </div>
-  </div>
+    <div class="card-container">
 
-    <div class="card">
-        <h2 class="name"> Ivysaur <h2>
-        <img class="card-img" src="https://i.pinimg.com/736x/5b/3e/f4/5b3ef42890e16fd39d8185c41577ec19.jpg">
-        <div class="card-body">
-            <h3>Grass/Poison</h3>
-            <p>Ivysaur uses sunlight to nourish the bud on its back which slowly grows larger as it stores energy.</p>
-            <p> Type 1: Grass </P>
-            <p> Type 2: Poison</P>
-            <p> Height 1.0m</P>
-            <p> Weight: 13kg </P>
-        <a class="button" href="https://pokemondb.net/pokedex/ivysaur"> More information </a>
-    </div>
-  </div>
+            <?php 
+            foreach ($data as $row):?>
+            <div class="card">
 
-    <div class="card">
-        <h2 class="name"> Bulbasaur <h2>
-        <img class="card-img" src="https://i.pinimg.com/736x/89/c6/00/89c60046db1da0bb99055eb55e599b11.jpg">
-        <div class="card-body">
-            <h3>Grass/Poison</h3>
-            <p>Bulbasaur carries a plant bulb on its back that grows using sunlight and nutrients absorbed from the ground.</p>
-            <p> Type 1: Grass </P>
-            <p> Type 2: Poison</P>
-            <p> Height: 0.7m </P>
-            <p> Weight: 6.9kg</P>
-            <a class="button" href="https://pokemondb.net/pokedex/bulbasaur"> More information </a>
-        </div>
-    </div>
+                <h2 class="name">
+                    <?= htmlspecialchars($row['name']) ?>
+                </h2>
 
-    <div class="card">
-        <h2 class="name"> Charmander <h2>
-        <img class="card-img" src="https://i.pinimg.com/736x/ba/8a/79/ba8a793afa1679a6dd4dc57d90c7d44f.jpg">
-        <div class="card-body">
-            <h3>Fire</h3>
-            <p>Charmander is a small reptile Pokémon whose tail flame indicates its health and emotional state.</p>
-            <p> Type 1: Fire</P>
-            <p> Type 2: -</P>
-            <p> Height: 0.6m </P>
-            <p> Weight: 8.5kg </P>
-        <a class="button" href="https://pokemondb.net/pokedex/charmander"> More information </a>
-        </div>
-     </div>
+                <img class="card-img"
+                    src="../pokemon img/icons/<?= (int)$row['dex_number'] ?>.png"
+                    alt="<?= htmlspecialchars($row['name']) ?>">
+                
+                <div class="card-body">
+                    <h3>
+                        <?=  htmlspecialchars($row['type1']) ?>
+                        <?=  $row ['type2'] ? '/'.htmlspecialchars($row['type2']) : '' ?>
+                    </h3>
+                    <p> type 1: <?= htmlspecialchars($row['type1']) ?></p>
+                    <p> type 2: <?=  htmlspecialchars($row['type2'] ?? '-') ?></p>
+                    <p> Height: <?=  htmlspecialchars($row['height']) ?> m </p>
+                    <p> weight: <?= htmlspecialchars($row['weight']) ?>kg </p>
 
-    <div class="card">
-        <h2 class="name"> Charmeleon <h2>
-        <img class="card-img" src="https://i.pinimg.com/736x/21/ff/b6/21ffb63af76de2d513a3ac9da4419f88.jpg">
-        <div class="card-body">
-            <h3>Fire</h3>
-            <p> Charmeleon is aggressive and constantly seeks battles while its tail flame burns fiercely. </p>
-            <p> Type 1: Fire </P>
-            <p> Type 2: -</P>
-            <p> Height: 1.1m</P>
-            <p> Weight: 19kg </P>
-            <a class="button" href="https://pokemondb.net/pokedex/charmeleon"> More information </a>
+                    <a class="button"
+                    href="catch.php?dex=<?=  (int)$row ['dex_number'] ?>"> probeer deze te vangen!
+                    </a>
+                </div>
             </div>
+            <?php endforeach; ?>
     </div>
-
-    <div class="card">
-        <h2 class="name"> Squirtle <h2>
-        <img class="card-img" src="https://i.pinimg.com/1200x/53/e2/1a/53e21afd8d22d17a975363767536029f.jpg">
-        <div class="card-body">
-            <h3>Water</h3>
-            <p>	Squirtle hides in its shell for protection and shoots strong jets of water from its mouth.</p>
-            <p> Type 1: Water</P>
-            <p> Type 2: -</P>
-            <p> Height: 0.5m </P>
-            <p> Weight 9kg</P>
-            <a href="https://pokemondb.net/pokedex/squirtle"> More information </a>
-    </div>
-  </div>
-
 </div>
-</div>
+            
 
 </body>
 </html>
